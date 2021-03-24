18d90004bc7c9df03c158ed1c76e39b4db8e45ca
CyrilGadara <gadaracyril11@gmail.com>
2021-03-24 19:44:09 +0530

    Update fetch.js

--- Blog/assets/scripts/modules/fetch.js
+++ Blog/assets/scripts/modules/fetch.js
@@ -3,7 +3,7 @@ import { blogTemplate } from './blog.js';
 
 function startLiveUpdate(){
     var blogData;
-        fetch('https://github.com/CyrilGadara/Project/blob/main/Blog/blog.json')
+        fetch('https://raw.githubusercontent.com/CyrilGadara/Project/main/Blog/blog.json')
         .then(function(response){
             return response.json();
         }).then(function(data){
